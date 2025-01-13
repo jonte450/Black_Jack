7582dbb0ab7a843059c03ea2c5c9bf9fca9af2d3
jonte450 <jonteyh@gmail.com>
2025-01-13 15:06:23 +0100

    My first card game with git-link

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    //https://github.com/jonte450/Black_Jack
+
     public static void main(String[] args) {
         Game game = new Game("Player 1");
       game.startGame();
